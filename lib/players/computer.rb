e55ed86ad9e035b9639a5493111cb57dfaf0983b
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-30 14:22:05 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -17,13 +17,16 @@ module Players
       return ["1", "3", "7", "9"].sample if board.turn_count == 1
       win = WIN_COMBINATIONS.find{|c| (board.cells[c[0]] == self.token && board.cells[c[1]] == self.token) || (board.cells[c[1]] == self.token && board.cells[c[2]] == self.token) || (board.cells[c[0]] == self.token && board.cells[c[2]] == self.token)}
       if win
+        puts "win = #{win}"
         return win.find{|pos| board.cells[pos] == " "}.to_s
       end
       lose = WIN_COMBINATIONS.find{|c| (board.cells[c[0]] == opponent && board.cells[c[1]] == opponent) || (board.cells[c[1]] == opponent && board.cells[c[2]] == opponent) || (board.cells[c[0]] == opponent && board.cells[c[2]] == opponent)}
       if lose
+        puts "lose = #{lose}"
         return lose.find{|pos| board.cells[pos] == " "}.to_s
       end
       comb = WIN_COMBINATIONS.find{|c| board.cells[c[0]] == self.token || board.cells[c[1]] == self.token || board.cells[c[2]] == self.token}
+      puts "comb = #{comb}"
       comb ? comb.sample.to_s : MOVES.sample
     end
   end
