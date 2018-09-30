88d23915dbcf70b708e00bf49b17ae9cfd761658
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-30 14:15:19 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -13,15 +13,15 @@ module Players
       lose = nil
       comb = nil
       self.token == "X" ? opponent = "O" : opponent = "X"
-      "5" if !board.taken?("5")
-      ["1", "3", "7", "9"].sample if board.turn_count == 1
+      return "5" if !board.taken?("5")
+      return ["1", "3", "7", "9"].sample if board.turn_count == 1
       win = WIN_COMBINATIONS.find{|c| (board.cells[c[0]] == self.token && board.cells[c[1]] == self.token) || (board.cells[c[1]] == self.token && board.cells[c[2]] == self.token) || (board.cells[c[0]] == self.token && board.cells[c[2]] == self.token)}
       if win
-        win.find{|pos| board.cells[pos] == " "}.to_s
+        return win.find{|pos| board.cells[pos] == " "}.to_s
       end
       lose = WIN_COMBINATIONS.find{|c| (board.cells[c[0]] == opponent && board.cells[c[1]] == opponent) || (board.cells[c[1]] == opponent && board.cells[c[2]] == opponent) || (board.cells[c[0]] == opponent && board.cells[c[2]] == opponent)}
       if lose
-        lose.find{|pos| board.cells[pos] == " "}.to_s
+        return lose.find{|pos| board.cells[pos] == " "}.to_s
       end
       comb = WIN_COMBINATIONS.find{|c| board.cells[c[0]] == self.token || board.cells[c[1]] == self.token || board.cells[c[2]] == self.token}
       comb ? comb.sample.to_s : MOVES.sample
